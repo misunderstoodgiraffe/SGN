03ff450355aaf5932028ed3c492f81ef4f458eac
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-06 19:07:17 -0700

    Update passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,10 +1,12 @@
 // server/config/passport.js
 
+//requires User model
+
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 
 // load up the user model
-// var User = require('../db/user');
+var User = require('../db/user');
 
 // load the auth variables
 var configAuth = require('./auth');
@@ -23,57 +25,48 @@ module.exports = function(passport) {
     });
   });
     
-    // code for login (use('local-login', new LocalStategy))
-    // code for signup (use('local-signup', new LocalStategy))
-    // code for facebook (use('facebook', new FacebookStrategy))
-    // code for twitter (use('twitter', new TwitterStrategy))
 
-    // =========================================================================
-    // GOOGLE ==================================================================
-    // =========================================================================
-    passport.use(new GoogleStrategy({
+  passport.use(new FacebookStrategy({
 
-        clientID        : configAuth.googleAuth.clientID,
-        clientSecret    : configAuth.googleAuth.clientSecret,
-        callbackURL     : configAuth.googleAuth.callbackURL,
-
-    },
+    clientID: configAuth.facebookAuth.clientID,
+    clientSecret: configAuth.facebookAuth.clientSecret,
+    callbackURL: configAuth.facebookAuth.callbackURL,
+  },
     function(token, refreshToken, profile, done) {
 
         // make the code asynchronous
         // User.findOne won't fire until we have all our data back from Google
-        process.nextTick(function() {
+      process.nextTick(function() {
 
-            // try to find the user based on their google id
-            User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                if (err)
-                    return done(err);
+        // try to find the user based on their google id
+        User.findOne({ id: profile.id }, function(err, user) {
+          if (err) {
+            return done(err);
+          }
 
-                if (user) {
+          if (user) {
+            // if a user is found, log them in
+            return done(null, user);
+          } else {
+            // if the user isnt in our database, create a new user
+            var newUser = new User();
 
-                    // if a user is found, log them in
-                    return done(null, user);
-                } else {
-                    // if the user isnt in our database, create a new user
-                    var newUser          = new User();
+            // set all of the relevant information
+            newUser.id = profile.id;
+            newUser.token = token;
+            newUser.name = profile.username;
+            newUser.email = profile.emails[0]; // pull the first email
 
-                    // set all of the relevant information
-                    newUser.google.id    = profile.id;
-                    newUser.google.token = token;
-                    newUser.google.name  = profile.displayName;
-                    newUser.google.email = profile.emails[0].value; // pull the first email
-
-                    // save the user
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
+            // save the user
+            newUser.save(function(err) {
+              if (err) {
+                throw err;
+              }
+              return done(null, newUser);
             });
+          }
         });
-
+      });
     }));
-
 };
 
