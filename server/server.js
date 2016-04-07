ae9838f6711a3435a75b79da1892d2004007d523
josh <jwentwor@gmail.com>
2016-04-07 14:18:16 -0700

    serve index.html

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var db = require('./db/db.js');
+var bodyParser = require('body-parser');
+db.dbConnection.connect()
 
 var app = express();
 
 // require('./routes.js') (app,express);
+app.use(express.static(__dirname + '/../client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(reg, res, next) {
+  res.redirect('index.html');
+});
 
 app.listen(3000);
 
