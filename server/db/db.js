c2bfa0645aca8cc570242893fddf9b5e6a5fda4b
josh <jwentwor@gmail.com>
2016-04-06 17:31:01 -0700

    slightly refactor db code

--- server/db/db.js
+++ server/db/db.js
@@ -1,19 +1,19 @@
 var mysql = require('mysql');
 
-module.exports.SQL = SQL = require('sequelize');
-module.exports.sql = sql = new SQL('users', 'root', 'm1sunderst00d', {define: {timestamps: false}});
+var SQL = require('sequelize');
+var sql = new SQL('users', 'root', 'm1sunderst00d', {define: {timestamps: false}});
 
-module.exports.dbConnection = mysql.createConnection({
+var dbConnection = mysql.createConnection({
   user: 'root',
   password: 'm1sunderst00d',
   database: 'users'
 });
 
-module.exports.Users = Users = sql.define('users', {
+var Users = sql.define('users', {
   username: SQL.STRING
 });
 
-module.exports.Friends = Friends = sql.define('friends', {
+var Friends = sql.define('friends', {
   userIdlink1: SQL.INT,
   userIdlink2: SQL.INT
 })
@@ -21,43 +21,30 @@ module.exports.Friends = Friends = sql.define('friends', {
 User.sync();
 Frinends.sync();
 
+// Skeleton code must be refactored for our schema
+
 module.exports.users = {
     getAll: function (callback) {
-      db.User.findAll().then(callback);
+      Users.findAll().then(callback);
     },
-    search: function (callback) {
-      db..findAll({include: [{
-        model: db.User,
-        where: {userId: db.SQL.col('Message.userId')}
-      }]}).then(function(result) {
-        var newResult = [];
-        for (var i in result) {
-          var username = result[i].dataValues.user.dataValues.username;
-          var newMessage = result[i].dataValues;
-          newMessage['username'] = username;
-          delete newMessage['user'];
-          newResult.push(newMessage);
-        }
-        
-        callback(newResult);
+    search: function (query, callback) {
+      Users.findAll({include: [{
+        model: Users,
+        where: query // {userId: db.SQL.col('Message.userId')}
+      }]}).then(function(result) {        
+        callback(result);
       });
     },
     newUser: function (user, callback) {
-      var newUser = db.User.build(user);
+      var newUser = Users.build(user);
       newUser.save().success(callback);
     }, // a function which produces all the messages
-    newFriend: function (user, callback) {
-      db.User.findAll({where: {username: message['username']}}).then( function(result) {
-        if (result.length === 0) {
-          var newUser = db.User.build({username: message['username']});
-          newUser.save().then(function(saveResults) {
-            postMessage(message, [{userId: saveResults.dataValues.id}], callback);
-          });
-        } else {
-          console.log(result);
-          postMessage(message, result, callback);
-        }
-      }); 
+    newFriend: function (user1, user2, callback) {
+      var newFriends = Friends.build({
+        userIdlink1: user1.id,
+        userIdlink2: user2.id
+      })
+      newFriends.save().success(callback);
     },
     searchFriends: function(callback) {}
   }
