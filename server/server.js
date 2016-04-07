7451db2caaf3c129dd5699373e95d2a630411699
josh <jwentwor@gmail.com>
2016-04-07 13:45:25 -0700

    fixing express

--- server/server.js
+++ server/server.js
@@ -3,10 +3,6 @@ var db = require('./db/db.js');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.redirect('index.html');
-});
-
 // require('./routes.js') (app,express);
 
 app.listen(3000);
