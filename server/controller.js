330a01b1711f0c7f4f06dbfb598a9c36d4573b31
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-07 19:02:51 -0700

    Add server route for user login

--- server/controller.js
+++ server/controller.js
@@ -3,6 +3,10 @@ var db = require('./db/db.js');
 module.exports = {
   checkAuth: function(){},
   signup: function(){},
-  addUser: function(){},
+  signin: function(req, res, next) {
+    console.log("getting here");
+    return res.json({data: "Some data"});
+  },
+  addUser: function() {},
   getFriends: function(){}
 }
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 var userController = require('./controller.js');
 
 module.exports = function (app, express) {
-  app.get('/signin', function(req, res) {
-    console.log('respond to signin request');
-    res.send("Success");
-  });
+  app.get('/signin', userController.signin);
+  // app.get('/signin', function(req, res) {
+  //   console.log('respond to signin request');
+  //   res.send("Success");
+  // });
 
 };
