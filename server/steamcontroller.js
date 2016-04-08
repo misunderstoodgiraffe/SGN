65cae9fa387b95ed52e5ebc13a674c587b9777df
Wilson Yu <wilsonweichengyu@gmail.com>
2016-04-08 14:42:16 -0700

    made steamController with working functions. need updates!

---
+++ server/steamcontroller.js
@@ -0,0 +1,82 @@
+/* 
+
+In the 4 functions getPlayerData, getAllFriends, getRecentGames, getAllGames
+
+All the on 'end' is just console.log atm. if we want to data manipulate in the future, we can change those specific lines.
+
+The format for taking in (in the exports) we will need the steamID passed in AS A STRINGIFIED number.
+
+The data currently displayed through console.log is just stringified data.
+
+*/
+
+
+
+
+var request = require('request');
+var key = require('./keys.js');
+
+var getPlayerData = function(steamID) {
+  var body = '';
+  var playerData = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + key.STEAM + '&steamids=' + steamID;
+  request.get(playerData)
+  .on('data', function(data) {
+    body += data;
+  })
+  .on('end', function (){
+    console.log(body);
+  });
+};
+
+var getAllFriends = function(steamID) {
+  var body = '';
+  var getAllFriendsLink = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=' + key.STEAM + '&steamid=' + steamID;
+  request.get(getAllFriendsLink)
+  .on('data', function(data) {
+    body+= data;
+  })
+  .on('end', function() {
+    console.log(body);
+  });
+};
+
+
+var getRecentGames = function (steamID) {
+  var body = '';
+  var recentGamesLink = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + key.STEAM + '&steamid=' + steamID;
+  request.get(recentGamesLink)
+  .on('data', function(data) {
+    body += data;
+  })
+  .on('end', function (){
+    console.log(body);
+  });
+};
+
+var getAllGames = function(steamID) {
+  var body = '';
+  var allGameData = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + key.STEAM + '&steamid=' + steamID;
+  request.get(allGameData)
+  .on('data', function(data) {
+    body += data;
+  })
+  .on('end', function (){
+    console.log(body);
+  });
+};
+
+
+
+// module.exports = {
+//   getFriendsList: function (req, res) {
+//     getFriendsList(steamID)
+//   }
+// };
+
+// getAllFriends('76561198082162743');
+
+// getRecentGames('76561198045493551');
+
+// getPlayerData('76561198045493551');
+
+// getAllGames('76561198045493551');
