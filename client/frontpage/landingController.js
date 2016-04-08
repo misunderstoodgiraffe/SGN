2953f6cb6df3e5244f301e819a5c59eb4712d61f
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-08 12:28:09 -0700

    Oauth returns an access token

--- client/frontpage/landingController.js
+++ client/frontpage/landingController.js
@@ -5,15 +5,15 @@ angular.module('SGN.landing', [])
   facebookService.initialize();
 
   //when the user clicks the connect twitter button, the popup authorization window opens
-  $scope.connectButton = function(event) {
-    console.log("clicking button");
-    event.preventDefault(event);
-    facebookService.connectFacebook().then(function(response) {
-      console.log(response.data);
-    }, function(error) {
-      console.log('error: ', error);
-    });
-  };
+  // $scope.connectButton = function(event) {
+  //   console.log("clicking button");
+  //   event.preventDefault(event);
+  //   facebookService.connectFacebook().then(function(response) {
+  //     console.log(response.data);
+  //   }, function(error) {
+  //     console.log('error: ', error);
+  //   });
+  // };
 
   //sign out clears the OAuth cache, the user will have to reauthenticate when returning
   $scope.signOut = function() {
--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,10 @@
 var userController = require('./controller.js');
 var OAuth = require('oauthio');
-OAuth.initialize('1730910463787032', '5b60ffc112a534c146203c0da965ebae');
+OAuth.initialize('BqQzmRHEA0gxZSaUVbqEaPmDyM8', 'hnjFc9WbVJuKigl8NmnVbjmD3jA');
 
 module.exports = function (app, express) {
   app.get('/oauth/redirect', OAuth.redirect(function(result, req, res) {
-    res.json({data: "some facebook data"});
+    res.json({data: result});
   }));
   app.get('/signin', OAuth.auth('facebook', 'http://localhost:3000/oauth/redirect'));
 
