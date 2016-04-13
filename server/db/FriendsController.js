fca6f9744ee5d980e6f90883cc12d3941e17a5ef
josh <jwentwor@gmail.com>
2016-04-12 20:00:26 -0700

    splitting up controller files

--- server/db/FriendsController.js
+++ server/db/FriendsController.js
@@ -1,41 +1,6 @@
 var db = require('./db.js');
 
 module.exports = {
-  getAllUsers: function (callback) {
-    db.Users.findAll().then(function(data) {
-      var results = [];
-      for (var i in data) {
-        results.push(data[i].dataValues);
-      }
-      callback(null, results);
-    }).catch(function(error) {
-      callback(error, null);
-    });
-  },
-  searchUsers: function (query, callback) {
-    db.Users.findAll({where: query})
-      .then(function(result) {
-        var results = [];
-        for (var i in result) {
-          results.push(result[i].dataValues);
-        }
-        callback(null, results);
-    }).catch(function(error) {
-      callback(error, null);
-    });
-  },
-  newUser: function (user, callback) {
-    var newUser = db.Users.create(user)
-    .then(function(data) {
-      callback(null, data.dataValues);
-    }).catch(function(error) {
-      if (error.errors) {
-        if (error.errors[0].message === 'fbID must be unique') {
-          callback(new Error('user already exists'), null);
-        } else {callback(error, null)}
-      } else {callback(error, null)}
-    });
-  },
   newFriend: function (user1, user2, callback) {
     if (user1.id === user2.id) {
       callback(new Error('cannot friend self'));
