e4d1a9a753852c799dbca59edbf6088426e23d6b
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-13 09:04:24 -0700

    Fix bugs in facebook controller

--- server/apis/fbController.js
+++ server/apis/fbController.js
@@ -8,39 +8,37 @@ module.exports = {
   getFacebookData: function(result, req, res) {
     if (result instanceof Error) {
       res.send(500, 'error: ' + result.message);
-    }
-    result.me().done(function(me) {
-      console.log(me.id);
+    } else {
+      result.me().done(function(me) {
 
-      //Add new user to database
-      db.newUser({fbID: me.id, givenName: me.name}, function(error, user) {
+        //Add new user to database
+        db.newUser({fbID: me.id, givenName: me.name, avatar: me.avatar}, function(error, user) {
 
-        if (error) { 
-          if (error.message === 'user already exists') { 
-            // user is already in database
-            db.searchUsers({fbID: me.id}, function(error, users) {
-              var user = users[0];
-              var token = jwt.encode(user, 'secret');
-              req.session.userJwtToken = token;
-              // !! Redirect to create profile page not home !! //
-              res.redirect('http://localhost:3000/#/home');
-            });
+          if (error) { 
+            if (error.message === 'user already exists') { 
+              // user is already in database
+              db.searchUsers({fbID: me.id}, function(error, users) {
+                var user = users[0];
+                var token = jwt.encode(user, 'secret');
+                res.redirect('http://localhost:3000/#/home');
+              });
+            } else {
+              // unknown error
+              res.send(500, error);
+            }
+
+            // New user
           } else {
-            // unknown error
-            res.send(500, error);
+            var token = jwt.encode(user, 'secret');
+            req.session.userJwtToken = token;
+            // !! Redirect to create profile page not home !! //
+            res.redirect('http://localhost:3000/#/home');
           }
 
-          // New user
-        } else {
-          var token = jwt.encode(user, 'secret');
-          req.session.userJwtToken = token;
-          // !! Redirect to create profile page not home !! //
-          res.redirect('http://localhost:3000/#/home');
-        }
-
-        
+          
+        });
       });
-    });
+    }
   },
 
 };
