28e710cc2e023c9b4ef9800ef6ad877ad810a8a5
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-13 15:42:43 -0700

    Separate the login page from angular. Allow user to access angular ONLY if logged in

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,14 +1,20 @@
-var userController = require('../apis/fbController.js');
+var fbController = require('../apis/fbController.js');
 var OAuth = require('oauthio');
 OAuth.initialize('BqQzmRHEA0gxZSaUVbqEaPmDyM8', 'hnjFc9WbVJuKigl8NmnVbjmD3jA');
 var userdb = require('../requestHandlers/users.js');
 
 module.exports = function (app, express) {
-  app.get('/', function(req, res) {
-    res.redirect('/welcome.html');
+  app.get('/', function(req, res, next) {
+    if (!req.session.userJwtToken) {
+      res.redirect('/welcome.html');
+      res.end();
+    } else {
+      console.log("redirect to dashboard");
+      next();
+    }
   });
   app.get('/oauth/redirect', OAuth.redirect(function(result, req, res) {
-    return userController.getFacebookData(result, req, res);
+    return fbController.getFacebookData(result, req, res);
   }));
   app.get('/signin', OAuth.auth('facebook', 'http://localhost:3000/oauth/redirect'));
   app.get('/users/profile', userdb.getProfile);
