805221b5b37699d63dd230e598297ecadda7dd69
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-18 16:32:03 -0700

    Add auth middleware

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,7 @@ OAuth.initialize('BqQzmRHEA0gxZSaUVbqEaPmDyM8', 'hnjFc9WbVJuKigl8NmnVbjmD3jA');
 var userdb = require('../requestHandlers/users.js');
 var steam = require('../apis/steamController.js');
 var key = require('./keys.js');
+var authCheck = require('./authMiddleware.js');
 
 module.exports = function (app, express) {
   app.get('/', function(req, res, next) {
@@ -24,31 +25,31 @@ module.exports = function (app, express) {
   app.get('/signout', userdb.signout);
   
   //USERS table
-  app.get('/users/profile', userdb.getProfile);
-  app.put('/users/profile', userdb.updateProfile);
+  app.get('/users/profile', authCheck, userdb.getProfile);
+  app.put('/users/profile', authCheck, userdb.updateProfile);
   
   //FRIENDS table
-  app.get('/users/friends', userdb.getFriends);
-  app.post('/users/friends', userdb.addFriend);
-  app.get('/users/addFriends', userdb.findFBFriends);
+  app.get('/users/friends', authCheck, userdb.getFriends);
+  app.post('/users/friends', authCheck, userdb.addFriend);
+  app.get('/users/addFriends', authCheck, userdb.findFBFriends);
   
   //STEAM table
-  app.get('/users/steam', userdb.getSteamProfile);
-  app.post('/users/steam', userdb.updateSteamProfile);
+  app.get('/users/steam', authCheck, userdb.getSteamProfile);
+  app.post('/users/steam', authCheck, userdb.updateSteamProfile);
 
   //GAMES table
-  app.get('/steam/games', userdb.getSteamGame);
+  app.get('/steam/games', authCheck, userdb.getSteamGame);
   app.post('/steam/games', userdb.addSteamGame);
 
   //USERSGAMES table
-  app.get('/users/games', userdb.getUserGameRelation);
-  app.post('/users/games', userdb.addUserGameRelation);
+  app.get('/users/games', authCheck, userdb.getUserGameRelation);
+  app.post('/users/games', authCheck, userdb.addUserGameRelation);
 
 
   //STEAM API ENDPOINTS
-  app.get('/updateSteam', steam.getPlayerData);
-  app.get('/updateSteamFriends', steam.getAllFriends);
-  app.get('/getSteamGames', steam.getAllGames);
-  app.get('/getGameInfo', steam.getGameInfo);
+  app.get('/updateSteam', authCheck, steam.getPlayerData);
+  app.get('/updateSteamFriends', authCheck, steam.getAllFriends);
+  app.get('/getSteamGames', authCheck, steam.getAllGames);
+  app.get('/getGameInfo', authCheck, steam.getGameInfo);
 
 };
